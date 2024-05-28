e438b1f8f601d9d9b98197e98b8ea3e527e2dbdf
Tomasz Nazar <tomeknazz@gmail.com>
2024-05-28 15:36:00 +0200

    Initial commit

---
+++ out/build/x64-Debug/CPM_modules/FindEigen.cmake
@@ -0,0 +1,3 @@
+include("C:/Users/tomek/Desktop/Studia/Podstawy Programowania/tp_projekt_4/out/build/x64-Debug/cmake/CPM_0.38.1.cmake")
+CPMAddPackage("NAME;Eigen;VERSION;3.4.0;URL;https://gitlab.com/libeigen/eigen/-/archive/3.4.0/eigen-3.4.0.tar.gz;DOWNLOAD_ONLY;YES")
+set(Eigen_FOUND TRUE)
